80e3ed1197ae5da5bf6f21549adafca0b8b7ae56
Robert Rothenberg <rrwo@cpan.org>
2016-01-12 15:24:40 +0000

    Update copyright year to 2016 [ci skip]

--- lib/Class/NonOO.pm
+++ lib/Class/NonOO.pm
@@ -188,7 +188,7 @@ Robert Rothenberg, C<< <rrwo at cpan.org> >>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2015 Robert Rothenberg.
+Copyright 2015-2016 Robert Rothenberg.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of the the Artistic License (2.0). You may obtain a
